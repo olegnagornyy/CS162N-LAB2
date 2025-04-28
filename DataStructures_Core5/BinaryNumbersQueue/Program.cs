c127462b861d00ac7c10e38565af087be8c8736a
Oleg Nagornyy <nagornyyo@my.lanecc.edu>
2025-04-27 22:33:15 -0700

    1

---
+++ DataStructures_Core5/BinaryNumbersQueue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BinaryNumbersQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queque = new Queue<string>();
+
+            int n = GetInt("The number you want to express as a binary number", 1, 1000);
+
+            queque.Enqueue("1");
+
+            for (int i = 1; i <= n; i++)
+            {
+                string next = queque.Dequeue();
+                queque.Enqueue(next + "0");
+                queque.Enqueue(next + "1");
+                Console.WriteLine(next);
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public static int GetInt(string label, int min, int max)
+        {
+            bool isInt = false;
+            int number = min - 1;
+            do
+            {
+                Console.Write(String.Format("Please enter a whole number between {0} and {1} for {2}: ", min, max, label));
+                string input = Console.ReadLine();
+                isInt = int.TryParse(input, out number);
+            } while (!(isInt && number >= min && number <= max));
+
+            return number;
+        }
+    }
+}
---
+++ DataStructures_Core5/StringReverseStack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringReverseStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            Console.WriteLine("Please enter a set of characters into the console. It will be printed in reverse.");
+
+            string input = Console.ReadLine();
+
+            for(int i = 0; i < input.Length; i++ )
+            {
+                char n = input[i];
+                stack.Push(n);
+            }
+
+            Console.WriteLine("Here is the output: ");
+                foreach (char n in stack)
+                    Console.Write(n + "");
+        }
+    }
+}
